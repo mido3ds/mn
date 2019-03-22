8ea6f7ebe18f98215aacee2baa7e03f7768b8648
Moustapha Saad <moustapha.saad.abdelhamed@gmail.com>
2019-03-22 02:26:52 +0200

    add generated projects and compile_commands

--- premake5.lua
+++ premake5.lua
@@ -1,7 +1,9 @@
+require "export-compile-commands"
+
 workspace "mn"
 	configurations {"debug", "release"}
 	platforms {"x86", "x64"}
-	location "build"
+	location ("projects/" .. _ACTION)
 	targetdir "bin/%{cfg.platform}/%{cfg.buildcfg}/"
 	startproject "unittest"
 	defaultplatform "x64"
