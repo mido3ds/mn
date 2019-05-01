d295b2617153c83f21c918d3fdc94ea1afa3addf
Moustapha Saad <moustapha.saad.abdelhamed@gmail.com>
2019-05-01 20:13:51 +0200

    add defer

---
+++ include/mn/Defer.h
@@ -0,0 +1,23 @@
+#pragma once
+
+namespace mn
+{
+	template <typename F>
+	struct Defer {
+		F f;
+		Defer(F f) : f(f) {}
+		~Defer() { f(); }
+	};
+
+	template<typename F>
+	inline static Defer<F>
+	make_defer(F f)
+	{
+		return Defer<F>(f);
+	}
+
+	#define mn_DEFER_1(x, y) x##y
+	#define mn_DEFER_2(x, y) mn_DEFER_1(x, y)
+	#define mn_DEFER_3(x)    mn_DEFER_2(x, __COUNTER__)
+	#define mn_defer(code)   auto mn_DEFER_3(_defer_) = make_defer([&](){code;})
+}
