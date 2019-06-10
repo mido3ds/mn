f34fcb84550cb5cfa013beaf79f558a3035ce15e
mohamedsh3lan <mohamed.shaalan@symbyo.com>
2019-06-10 15:19:08 +0200

    logger interface

--- mn/include/mn/Logger.h
+++ mn/include/mn/Logger.h
@@ -6,24 +6,28 @@
 
 namespace mn
 {
-	struct Logger
-	{
-		Stream current_stream;
-		Mutex mtx;
-	};
+	struct Logger;
 
-	 
 	template<typename ... TArgs>
 	inline static size_t
 	log(const char* message, TArgs&& ... args)
 	{
-		mutex_lock(logger.mtx);
+		mutex_lock(get_logger_instance()->mtx);
 
-		vprintf(logger.current_stream, message, std::forward<TArgs>(args)...);
+		vprintf(log_stream_get(), message, std::forward<TArgs>(args)...);
 
-		mutex_unlock(logger.mtx);
+		mutex_unlock(get_logger_instance()->mtx);
 	}
 
+	MN_EXPORT Logger*
+	get_logger_instance();
+
 	MN_EXPORT void
-	log_stream_change(const Stream& stream);
+	logger_free();
+
+	MN_EXPORT void
+	log_stream_change(Stream& stream);
+
+	MN_EXPORT Stream
+	log_stream_get();
 }
--- mn/src/mn/winos/Logger.cpp
+++ mn/src/mn/winos/Logger.cpp
@@ -3,14 +3,35 @@
 
 namespace mn
 {
-	static Logger logger;
+	struct Logger
+	{
+		Stream current_stream;
+		Mutex mtx;
+	};
+
+	Logger* get_logger_instance()
+	{
+		static Logger _log = Logger{ stream_stderr(), mutex_new("log mutex") };
+		return &_log;
+	}
+
+	void logger_free()
+	{
+		stream_free(get_logger_instance()->current_stream);
+		mutex_free(get_logger_instance()->mtx);
+	}
 
 	void log_stream_change(Stream& stream)
 	{
-		mutex_lock(logger.mtx);
+		mutex_lock(get_logger_instance()->mtx);
 
-		logger.current_stream = stream;
+		get_logger_instance()->current_stream = stream;
 
-		mutex_unlock(logger.mtx);
+		mutex_unlock(get_logger_instance()->mtx);
+	}
+
+	Stream log_stream_get()
+	{
+		return get_logger_instance()->current_stream;
 	}
 }
