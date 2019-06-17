bc94c2e7905030561f2f0646cc86a0f7a5788f6f
hanaa-mohamed <hanaa.ghobashi@symbyo.com>
2019-06-17 17:07:36 +0200

    Solve destruction order problem

--- mn/src/mn/Memory.cpp
+++ mn/src/mn/Memory.cpp
@@ -2,63 +2,60 @@
 #include "mn/Buf.h"
 #include "mn/memory/Leak.h"
 
-#if DEBUG
-	#define ENABLE_LEAK_DETECTOR 1
-#else
-	#define ENABLE_LEAK_DETECTOR 0
-#endif
-
 namespace mn
 {
 	//allocator stack interface
 	struct Allocator_Stack_Wrapper
 	{
-		Buf<Allocator> stack;
-
-		Allocator_Stack_Wrapper()
-		{
-			stack = buf_with_allocator<Allocator>(memory::clib());
-		}
-
-		~Allocator_Stack_Wrapper()
-		{
-			buf_free(stack);
-		}
+		inline static constexpr size_t CAPACITY = 1024;
+		Allocator stack[CAPACITY];
+		int allocator_index = -1;
 	};
 
-	inline static Buf<Allocator>&
-	_allocator_stack()
+	inline static Allocator_Stack_Wrapper&
+	_allocator_wrapper()
 	{
 		thread_local Allocator_Stack_Wrapper _wrapper;
-		return _wrapper.stack;
+		return _wrapper;
+	}
+
+	inline static Allocator*
+	_allocator_stack()
+	{
+		return _allocator_wrapper().stack;
+	}
+
+	inline static int&
+	_allocator_index()
+	{
+		return _allocator_wrapper().allocator_index;
 	}
 
 	Allocator
 	allocator_top()
 	{
-		Buf<Allocator>& stack = _allocator_stack();
-		if (stack.count == 0)
+		int index = _allocator_index();
+		if (index < 0)
 		{
-			#if ENABLE_LEAK_DETECTOR
-				return memory::leak();
-			#else
-				return memory::clib();
-			#endif
+			return memory::clib();
 		}
-		return buf_top(stack);
+
+		return _allocator_stack()[index];
 	}
 
 	void
 	allocator_push(Allocator allocator)
 	{
-		Buf<Allocator>& stack = _allocator_stack();
-		buf_push(stack, allocator);
+		Allocator* stack = _allocator_stack();
+		int& allocator_index = _allocator_index();
+		allocator_index++;
+		stack[allocator_index] = allocator;
 	}
 
 	void
 	allocator_pop()
 	{
-		Buf<Allocator>& stack = _allocator_stack();
-		buf_pop(stack);
+		int& allocator_index = _allocator_index();
+		allocator_index--;
 	}
 }
