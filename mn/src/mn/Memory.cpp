8b23aa217186f8488d0c7f93448bd4a00a93fcfa
Hanaa Mohamed <hanaa.ghobashi@symbyo.com>
2019-06-20 10:52:15 +0200

    NDPS-389: Fix after review

--- mn/src/mn/Memory.cpp
+++ mn/src/mn/Memory.cpp
@@ -2,6 +2,8 @@
 #include "mn/Buf.h"
 #include "mn/memory/Leak.h"
 
+#include <assert.h>
+
 #if DEBUG
 	#define ENABLE_LEAK_DETECTOR 1
 #else
@@ -11,36 +13,24 @@
 namespace mn
 {
 	//allocator stack interface
-	struct Allocator_Stack_Wrapper
+	struct Allocator_Stack
 	{
 		inline static constexpr size_t CAPACITY = 1024;
-		Allocator stack[CAPACITY];
-		int allocator_index = -1;
+		Allocator _stack[CAPACITY];
+		int _index = -1;
 	};
 
-	inline static Allocator_Stack_Wrapper&
-	_allocator_wrapper()
-	{
-		thread_local Allocator_Stack_Wrapper _wrapper;
-		return _wrapper;
-	}
-
-	inline static Allocator*
+	inline static Allocator_Stack&
 	_allocator_stack()
 	{
-		return _allocator_wrapper().stack;
-	}
-
-	inline static int&
-	_allocator_index()
-	{
-		return _allocator_wrapper().allocator_index;
+		thread_local Allocator_Stack stack;
+		return stack;
 	}
 
 	Allocator
 	allocator_top()
 	{
-		int index = _allocator_index();
+		int index = _allocator_stack()._index;
 		if (index < 0)
 		{
 			#if ENABLE_LEAK_DETECTOR
@@ -50,21 +40,23 @@ namespace mn
 			#endif
 		}
 
-		return _allocator_stack()[index];
+		return _allocator_stack()._stack[index];
 	}
 
 	void
 	allocator_push(Allocator allocator)
 	{
-		Allocator* stack = _allocator_stack();
-		int& allocator_index = _allocator_index();
-		stack[++allocator_index] = allocator;
+		Allocator* stack = _allocator_stack()._stack;
+		int& index = _allocator_stack()._index;
+		assert(index < Allocator_Stack::CAPACITY);
+		stack[++index] = allocator;
 	}
 
 	void
 	allocator_pop()
 	{
-		int& allocator_index = _allocator_index();
-		--allocator_index;
+		int& index = _allocator_stack()._index;
+		assert(index >= 0);
+		--index;
 	}
 }
