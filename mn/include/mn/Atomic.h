2c02644930f0e14a76683d50d914a408c8ad9570
Moustapha Saad <moustapha.saad.abdelhamed@gmail.com>
2019-07-27 11:47:14 +0200

    add windows implementation to atomics

--- mn/include/mn/Atomic.h
+++ mn/include/mn/Atomic.h
@@ -13,10 +13,30 @@ namespace mn
 	MN_EXPORT int64_t
 	atomic_inc(int64_t &self);
 
+	//increments the variable and returns the value AFTER dec
+	MN_EXPORT int32_t
+	atomic_dec(int32_t &self);
+
+	MN_EXPORT int64_t
+	atomic_dec(int64_t &self);
+
 	//adds value to the variable and returns the value BEFORE add
 	MN_EXPORT int32_t
 	atomic_add(int32_t &self, int32_t value);
 
 	MN_EXPORT int64_t
 	atomic_add(int64_t &self, int64_t value);
+
+	//returns the value BEFORE the exchange
+	MN_EXPORT int32_t
+	atomic_exchange(int32_t &self, int32_t value);
+
+	MN_EXPORT int64_t
+	atomic_exchange(int64_t &self, int64_t value);
+
+	MN_EXPORT int32_t
+	atomic_load(int32_t &self);
+
+	MN_EXPORT int64_t
+	atomic_load(int64_t &self);
 }
--- mn/src/mn/winos/Atomic.cpp
+++ mn/src/mn/winos/Atomic.cpp
@@ -23,6 +23,22 @@ namespace mn
 	}
 
 	int32_t
+	atomic_dec(int32_t &self)
+	{
+		static_assert(sizeof(LONG) == sizeof(int32_t));
+		assert(uintptr_t(&self) % sizeof(self) == 0 && "atomic is not aligned");
+		return InterlockedDecrement((LONG*)&self);
+	}
+
+	int64_t
+	atomic_dec(int64_t &self)
+	{
+		static_assert(sizeof(LONG64) == sizeof(int64_t));
+		assert(uintptr_t(&self) % sizeof(self) == 0 && "atomic is not aligned");
+		return InterlockedDecrement64((LONG64*)&self);
+	}
+
+	int32_t
 	atomic_add(int32_t &self, int32_t value)
 	{
 		static_assert(sizeof(LONG) == sizeof(int32_t));
@@ -36,5 +52,37 @@ namespace mn
 		static_assert(sizeof(LONG64) == sizeof(int64_t));
 		assert(uintptr_t(&self) % sizeof(self) == 0 && "atomic is not aligned");
 		return InterlockedExchangeAdd64((LONG64*)&self, LONG64(value));
+	}
+
+	int32_t
+	atomic_exchange(int32_t &self, int32_t value)
+	{
+		static_assert(sizeof(LONG) == sizeof(int32_t));
+		assert(uintptr_t(&self) % sizeof(self) == 0 && "atomic is not aligned");
+		return InterlockedExchange((LONG*)&self, LONG(value));
+	}
+
+	int64_t
+	atomic_exchange(int64_t &self, int64_t value)
+	{
+		static_assert(sizeof(LONG64) == sizeof(int64_t));
+		assert(uintptr_t(&self) % sizeof(self) == 0 && "atomic is not aligned");
+		return InterlockedExchange64((LONG64*)&self, LONG64(value));
+	}
+
+	int32_t
+	atomic_load(int32_t &self)
+	{
+		static_assert(sizeof(LONG) == sizeof(int32_t));
+		assert(uintptr_t(&self) % sizeof(self) == 0 && "atomic is not aligned");
+		return InterlockedOr((LONG*)&self, LONG(0));
+	}
+
+	int64_t
+	atomic_load(int64_t &self)
+	{
+		static_assert(sizeof(LONG64) == sizeof(int64_t));
+		assert(uintptr_t(&self) % sizeof(self) == 0 && "atomic is not aligned");
+		return InterlockedOr64((LONG64*)&self, LONG64(0));
 	}
 }
