81f2bb352ce8c6d24f7c59e9adcd0e69232ad53a
Moustapha Saad <moustapha.saad.abdelhamed@gmail.com>
2020-01-13 12:28:25 +0200

    add file lock example

---
+++ examples/example-file-lock-increment.cpp
@@ -0,0 +1,37 @@
+#include <mn/IO.h>
+#include <mn/File.h>
+#include <mn/Defer.h>
+#include <mn/Thread.h>
+#include <mn/Path.h>
+
+int
+main()
+{
+	auto f = mn::file_open("koko.bin", mn::IO_MODE::READ_WRITE, mn::OPEN_MODE::CREATE_OVERWRITE);
+	mn_defer(mn::file_close(f));
+
+	uint64_t v = 0;
+	mn::file_cursor_move_to_start(f);
+	mn::file_write(f, mn::block_from(v));
+
+	while(true)
+	{
+		mn::file_cursor_move_to_start(f);
+
+		mn::file_write_lock(f, 0, 8);
+
+		auto r = mn::file_read(f, mn::block_from(v));
+		assert(r == 8);
+		++v;
+		mn::file_cursor_move_to_start(f);
+		r = mn::file_write(f, mn::block_from(v));
+		assert(r == 8);
+
+		mn::file_write_unlock(f, 0, 8);
+
+		mn::print("Version '{}'\n", v);
+		mn::thread_sleep(1000);
+	}
+
+	return 0;
+}
---
+++ examples/example-file-lock-reset.cpp
@@ -0,0 +1,33 @@
+#include <mn/IO.h>
+#include <mn/File.h>
+#include <mn/Defer.h>
+#include <mn/Thread.h>
+#include <mn/Path.h>
+
+int
+main()
+{
+	auto o = mn::file_open("other.bin", mn::IO_MODE::READ_WRITE, mn::OPEN_MODE::CREATE_OVERWRITE);
+
+	auto f = mn::file_open("koko.bin", mn::IO_MODE::READ_WRITE, mn::OPEN_MODE::OPEN_ONLY);
+
+	mn::file_write_lock(f, 0, 8);
+
+	uint64_t v = 0;
+	auto r = mn::file_read(f, mn::block_from(v));
+	assert(r == 8);
+
+	mn::file_write_unlock(f, 0, 8);
+
+	mn::print("Version '{}'\n", v);
+
+	v = 0;
+	mn::file_write(o, mn::block_from(v));
+
+	mn::file_remove("koko.bin");
+	mn::file_move("other.bin", "koko.bin");
+
+	mn::file_close(o);
+	mn::file_close(f);
+	return 0;
+}
