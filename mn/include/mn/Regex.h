c75e56ab3cf2a7bde4e7a1d9ec1ebde826333a76
MoustaphaSaad <moustapha.saad.abdelhamed@gmail.com>
2021-03-13 10:04:20 +0200

    document regex interface

--- mn/include/mn/Regex.h
+++ mn/include/mn/Regex.h
@@ -7,6 +7,17 @@
 
 namespace mn
 {
+	// A Simple Regex Engine
+	// this is a simple implementation of a regex engine which contains all the regex operators
+	// that i need and use, and they are
+	// '.': matches any rune
+	// '|': or operator
+	// '*': zero or more operator along with the non greedy variant '*?'
+	// '+': one or more operator along with the non greedy variant '+?'
+	// '?': optional operator along with the non greedy variant '??'
+	// '[]': in-set operator along with the not-in-set '[^]' and ranges '[a-z]'
+
+	// defines the operators that's supported by the regex virtual machine
 	enum RGX_OP: uint8_t
 	{
 		// [RUNE, 'a']: matches a rune
@@ -30,17 +41,20 @@ namespace mn
 		RGX_OP_MATCH2,
 	};
 
+	// a compiled regex
 	struct Regex
 	{
 		Buf<uint8_t> bytes;
 	};
 
+	// creates a new empty regex program
 	inline static Regex
 	regex_new()
 	{
 		return Regex{};
 	}
 
+	// frees a regex program
 	inline static void
 	regex_free(Regex& self)
 	{
@@ -53,6 +67,7 @@ namespace mn
 		regex_free(self);
 	}
 
+	// clones a regex program
 	inline static Regex
 	regex_clone(const Regex& other, Allocator allocator = allocator_top())
 	{
@@ -74,9 +89,13 @@ namespace mn
 		int32_t payload;
 	};
 
+	// compiles a regular expression into a regex program
 	MN_EXPORT Result<Regex>
 	regex_compile(Regex_Compile_Unit unit);
 
+	// compiles a regular expression into a regex program with a match2 opcode at the end
+	// this is useful in case you want to identify which match did happen, in case you have
+	// different matches like (abc|def) and want to identify whether the match is abc or def
 	inline static Result<Regex>
 	regex_compile_with_payload(const Str& str, int32_t payload, Allocator allocator = allocator_top())
 	{
@@ -94,6 +113,7 @@ namespace mn
 		return regex_compile_with_payload(str_lit(str), payload, allocator);
 	}
 
+	// compiles a regular expression into a regex program
 	inline static Result<Regex>
 	regex_compile(const Str& str, Allocator allocator = allocator_top())
 	{
@@ -110,6 +130,11 @@ namespace mn
 	}
 
 	// regex vm
+
+	// match result contains the range of the match in the string and whether it's a match or not
+	// because in case of mismatch the begin, end pointers will have how far has the VM has looked
+	// which means that you can discard this sub string because it won't match, this is used in search
+	// also we have the payload thing mentioned above
 	struct Match_Result
 	{
 		const char* begin;
@@ -119,9 +144,11 @@ namespace mn
 		int32_t payload;
 	};
 
+	// tries to match the compiled regex program to the given string
 	MN_EXPORT Match_Result
 	regex_match(const Regex& program, const char* str);
 
+	// search for the first match of the regex program in the given string
 	MN_EXPORT Match_Result
 	regex_search(const Regex& program, const char* str);
 }
