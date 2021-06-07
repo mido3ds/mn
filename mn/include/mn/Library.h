5ae6c5883b08ca02bb1093cc85767f32c7b5c8f8
MoustaphaSaad <moustapha.saad.abdelhamed@gmail.com>
2021-06-07 17:48:13 +0200

    document Library.h

--- mn/include/mn/Library.h
+++ mn/include/mn/Library.h
@@ -5,29 +5,36 @@
 
 namespace mn
 {
+	// a library handle
 	typedef void* Library;
 
+	// tries to open the library at the given path, if it fails it will return a nullptr
 	MN_EXPORT Library
 	library_open(const Str& filename);
 
+	// tries to open the library at the given path, if it fails it will return a nullptr
 	inline static Library
 	library_open(const char* filename)
 	{
 		return library_open(str_lit(filename));
 	}
 
+	// closes the given library
 	MN_EXPORT void
 	library_close(Library self);
 
+	// destruct overload for library close
 	inline static void
 	destruct(Library self)
 	{
 		library_close(self);
 	}
 
+	// searches for a procedure with the given name inside the library
 	MN_EXPORT void*
 	library_proc(Library self, const Str& proc_name);
 
+	// searches for a procedure with the given name inside the library
 	inline static void*
 	library_proc(Library self, const char* proc_name)
 	{
