e0aa31cddaa749560f60e31b12abc35cd4ab802a
MoustaphaSaad <moustapha.saad.abdelhamed@gmail.com>
2021-06-07 20:23:14 +0200

    document Pool.h

--- mn/include/mn/Pool.h
+++ mn/include/mn/Pool.h
@@ -7,56 +7,30 @@
 
 namespace mn
 {
-	/**
-	 * Pool handle
-	 */
+	// memory pool handle
 	typedef struct IPool* Pool;
 
-	/**
-	 * @brief      Creates a new memory pool
-	 * A Memory pool is a list of buckets with `bucket_size` capacity which it uses to hand
-	 * over a memory piece with the given `element_size` every time you call `pool_get`
-	 *
-	 * @param[in]  element_size    The element size (in bytes)
-	 * @param[in]  bucket_size     The bucket size (in elements count)
-	 * @param[in]  meta_allocator  The meta allocator for the pool to allocator from
-	 */
+	// creates a new memory pool for the given element size, internally the pool uses buckets of the
+	// given bucket_size of elements and using the meta allocator to allocate more memory
 	MN_EXPORT Pool
 	pool_new(size_t element_size, size_t bucket_size, Allocator meta_allocator = allocator_top());
 
-	/**
-	 * @brief      Frees the memory pool
-	 *
-	 * @param[in]  pool  The pool
-	 */
+	// frees the given memory pool
 	MN_EXPORT void
 	pool_free(Pool pool);
 
-	/**
-	 * @brief      Destruct function overload for the pool
-	 *
-	 * @param[in]  pool  The pool
-	 */
+	// destruct overload for pool free
 	inline static void
 	destruct(Pool pool)
 	{
 		pool_free(pool);
 	}
 
-	/**
-	 * @brief      Returns a memory suitable to write an object of size `element_size` in
-	 *
-	 * @param[in]  pool  The pool
-	 */
+	// returns a memory suitable to write an object of size element_size used in creation function
 	MN_EXPORT void*
 	pool_get(Pool pool);
 
-	/**
-	 * @brief      Puts back the given memory into the pool to be reused later
-	 *
-	 * @param[in]  pool  The pool
-	 * @param      ptr   The pointer to memory
-	 */
+	// puts back the given memory into the pool to be reused later
 	MN_EXPORT void
 	pool_put(Pool pool, void* ptr);
 }
