2df80e2cd9a8baa7731bff92e138098de7777f7f
MoustaphaSaad <moustapha.saad.abdelhamed@gmail.com>
2021-06-07 19:56:26 +0200

    document Memory_Stream.h

--- mn/include/mn/Memory_Stream.h
+++ mn/include/mn/Memory_Stream.h
@@ -6,9 +6,11 @@
 
 namespace mn
 {
+	// an in memory byte stream
 	typedef struct IMemory_Stream* Memory_Stream;
 
-	// just forward declaration because this language require this kind of thing
+	// some forward declaration because this language require this kind of thing
+
 	MN_EXPORT void
 	memory_stream_cursor_move(Memory_Stream self, int64_t offset);
 
@@ -58,174 +60,93 @@ namespace mn
 		}
 	};
 
-	/**
-	 * @brief      Creates new memory stream
-	 *
-	 * @param[in]  allocator  The allocator to be used (optional by default will take
-	 * whatever on top of the allocator stack)
-	 */
+	// creates a new memory stream with the given allocator
 	MN_EXPORT Memory_Stream
 	memory_stream_new(Allocator allocator = allocator_top());
 
-	/**
-	 * @brief      Frees the memory stream
-	 *
-	 * @param      self  The memory stream
-	 */
+	// frees the given memory stream
 	MN_EXPORT void
 	memory_stream_free(Memory_Stream self);
 
-	/**
-	 * @brief      Destruct function overload for the memory stream
-	 *
-	 * @param      self  The memory stream
-	 */
+	// destruct overload for memory stream free
 	inline static void
 	destruct(Memory_Stream self)
 	{
 		memory_stream_free(self);
 	}
 
-	/**
-	 * @brief      Writes the given block in the memory stream
-	 *
-	 * @param      self  The memory stream
-	 * @param[in]  data  The data block
-	 *
-	 * @return     The size of the written data in bytes
-	 */
+	// writes the given block of bytes to a memory stream, returns the amount of written bytes
 	MN_EXPORT size_t
 	memory_stream_write(Memory_Stream self, Block data);
 
-	/**
-	 * @brief      Reads into the given block from the memory stream
-	 *
-	 * @param      self  The memory stream
-	 * @param[in]  data  The data block
-	 *
-	 * @return     The size of the read data in bytes
-	 */
+	// reads into the given memory block from a memory stream, returns the amount of read bytes
 	MN_EXPORT size_t
 	memory_stream_read(Memory_Stream self, Block data);
 
-	/**
-	 * @brief      Returns the size of the memory stream in bytes
-	 *
-	 * @param      self  The memory stream
-	 */
+	// returns the size of the given memory stream
 	MN_EXPORT int64_t
 	memory_stream_size(Memory_Stream self);
 
+	// returns whether stream's cursor has reached end of file
 	MN_EXPORT bool
 	memory_stream_eof(Memory_Stream self);
 
-	/**
-	 * @brief      Returns the position of the cursor in the memory stream
-	 *
-	 * @param      self  The memory stream
-	 */
+	// returns the position of stream's cursor
 	MN_EXPORT int64_t
 	memory_stream_cursor_pos(Memory_Stream self);
 
-	/**
-	 * @brief      Moves the cursor by the given offset
-	 *
-	 * @param      self    The memory stream
-	 * @param[in]  offset  The offset
-	 */
+	// moves the cursor by the given offset
 	MN_EXPORT void
 	memory_stream_cursor_move(Memory_Stream self, int64_t offset);
 
-	/**
-	 * @brief      Moves the cursor to the given absolute position
-	 *
-	 * @param      self  The memory stream
-	 * @param[in]  abs   The absolute position
-	 */
+	// sets the cursor to the given absolute position
 	MN_EXPORT void
 	memory_stream_cursor_set(Memory_Stream self, int64_t abs);
 
-	/**
-	 * @brief      Moves the cursor to the start of the memory stream
-	 *
-	 * @param      self  The memory stream
-	 */
+	// sets stream's cursor to the start
 	MN_EXPORT void
 	memory_stream_cursor_to_start(Memory_Stream self);
 
-	/**
-	 * @brief      Moves the cursor to the end of the memory stream
-	 *
-	 * @param      self  The memory stream
-	 */
+	// sets stream's cursor to the end
 	MN_EXPORT void
 	memory_stream_cursor_to_end(Memory_Stream self);
 
-	/**
-	 * @brief      Ensures the memory stream has the capacity to hold the given size(in bytes)
-	 *
-	 * @param      self  The memory stream
-	 * @param[in]  size  The size (in bytes)
-	 */
+	// ensures that stream's capacity can accomodate the given size of bytes
 	MN_EXPORT void
 	memory_stream_reserve(Memory_Stream self, size_t size);
 
-	/**
-	 * @brief      Returns the capacity of the memory stream
-	 *
-	 * @param      self  The memory stream
-	 */
+	// returns the capacity of the given stream
 	MN_EXPORT size_t
 	memory_stream_capacity(Memory_Stream self);
 
-	/**
-	 * @brief      Clears the memory stream
-	 *
-	 * @param      self  The memory stream
-	 */
+	// clears the given memory stream
 	MN_EXPORT void
 	memory_stream_clear(Memory_Stream self);
 
-	/**
-	 * @brief      Returns the memory block starting from the cursor with the given
-	 * size(in bytes) advancing towards the end of the stream
-	 * memory_stream_block_ahead([abcd|efghe], 2) -> [ef]
-	 *
-	 * @param      self  The memory stream
-	 * @param[in]  size  The size (in bytes)
-	 */
+	// retursn the memory block starting from the cursor with the given size(in bytes) advancing
+	// towards the end of the stream
+	// memory_stream_block_ahead([abcd|efghe], 2) -> [ef]
 	MN_EXPORT Block
 	memory_stream_block_ahead(Memory_Stream self, size_t size);
 
-	/**
-	 * @brief      Returns the memory block starting from the cursor with the given
-	 * size(in bytes) advancing towards the start of the stream
-	 * memory_stream_block_behind([abcd|efghe], 2) -> [cd]
-	 *
-	 * @param      self  The memory stream
-	 * @param[in]  size  The size (in bytes)
-	 */
+	// returns the memory block starting from the cursor with the given
+	// size(in bytes) advancing towards the start of the stream
+	// memory_stream_block_behind([abcd|efghe], 2) -> [cd]
 	MN_EXPORT Block
 	memory_stream_block_behind(Memory_Stream self, size_t size);
 
-	/**
-	 * @brief      Pipes data into the memory stream with the given size
-	 *
-	 * @param      self    The memory stream to pipe the data into
-	 * @param[in]  stream  The stream to pipe the data from
-	 * @param[in]  size    The size(in bytes) of the data to be piped
-	 *
-	 * @return     The size of the piped data(in bytes)
-	 */
+	// pipes data into the memory stream with the given size, returns the amount of written bytes
 	MN_EXPORT size_t
 	memory_stream_pipe(Memory_Stream self, Stream stream, size_t size);
 
+	// returns a pointer to the content of the memory stream
 	inline static const char*
 	memory_stream_ptr(Memory_Stream self)
 	{
 		return self->str.ptr;
 	}
 
+	// returns the content of the memory stream as a string, and clears the given memory stream
 	inline static Str
 	memory_stream_str(Memory_Stream self)
 	{
