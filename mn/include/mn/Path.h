9319582df6f3a5beb94bcd599c13d470d6112c5f
MoustaphaSaad <moustapha.saad.abdelhamed@gmail.com>
2021-06-07 20:20:17 +0200

    document path.h

--- mn/include/mn/Path.h
+++ mn/include/mn/Path.h
@@ -6,18 +6,11 @@
 
 namespace mn
 {
-	/**
-	* @brief      A Helper function which given the filename will load the content of it
-	* into the resulting string
-	*
-	* @param[in]  filename   The filename
-	* @param[in]  allocator  The allocator to be used by the resulting string
-	*
-	* @return     A String containing the content of the file
-	*/
+	// a helper function which given the filename will load the content of it into the resulting string
 	MN_EXPORT Str
 	file_content_str(const char* filename, Allocator allocator = allocator_top());
 
+	// a helper function which given the filename will load the content of it into the resulting string
 	inline static Str
 	file_content_str(const Str& filename, Allocator allocator = allocator_top())
 	{
@@ -25,39 +18,26 @@ namespace mn
 	}
 
 
-	//File System api
-	/**
-	 * @brief      Converts from standard encoding(linux-like) to os-specific encoding
-	 *
-	 * @param[in]  path  The path
-	 *
-	 * @return     A String containing the encoded path
-	 */
+	// converts from standard encoding (linux-like) to is specific encoding
+	// you probably won't need to use this function because mn takes care of conversions internally
+	// it might be of use if you're writing os specific code outside of mn
 	MN_EXPORT Str
 	path_os_encoding(const char* path, Allocator allocator = memory::tmp());
 
-	/**
-	 * @brief      Converts from standard encoding(linux-like) to os-specific encoding
-	 *
-	 * @param[in]  path  The path
-	 *
-	 * @return     A String containing the encoded path
-	 */
+	// converts from standard encoding (linux-like) to is specific encoding
+	// you probably won't need to use this function because mn takes care of conversions internally
+	// it might be of use if you're writing os specific code outside of mn
 	inline static Str
 	path_os_encoding(const Str& path, Allocator allocator = memory::tmp())
 	{
 		return path_os_encoding(path.ptr, allocator);
 	}
 
-	/**
-	 * Removes duplicate / and other sanitization stuff
-	 */
+	// removes duplicate / and other sanitization stuff
 	MN_EXPORT Str
 	path_sanitize(Str path);
 
-	/**
-	 * Converts from the os-specific encoding to standard encoding(linux-like)
-	 */
+	// converts from os specific encoding to standard encoding (linux-like)
 	MN_EXPORT Str
 	path_normalize(Str path);
 
@@ -67,12 +47,9 @@ namespace mn
 		return path_sanitize(base);
 	}
 
-	/**
-	 * @brief      Joins multiple folders in the path
-	 * base = "base_folder"
-	 * base = path_join(base, "my_folder1", "my_folder2", "my_file") ->
-	 * base_folder/my_folder1/my_folder2/my_file
-	 */
+	// joins multiple path fragments together in the path
+	// base = "base_folder"
+	// base = path_join(base, "my_folder1", "my_folder2", "my_file") = "base_folder/my_folder1/my_folder2/my_file"
 	template<typename TFirst, typename ... TArgs>
 	inline static Str
 	path_join(Str base, TFirst&& first, TArgs&& ... args)
@@ -83,108 +60,77 @@ namespace mn
 		return path_join(base, std::forward<TArgs>(args)...);
 	}
 
-	/**
-	 * @brief      Returns whether the path exists or not
-	 */
+	// returns whether the path exists
 	MN_EXPORT bool
 	path_exists(const char* path);
 
-	/**
-	 * @brief      Returns whether the path exists or not
-	 */
+	// returns whether the path exists
 	inline static bool
 	path_exists(const Str& path)
 	{
 		return path_exists(path.ptr);
 	}
 
-	/**
-	 * @brief      Returns whether the given path is a folder or not
-	 */
+	// returns whether the given path is a folder
 	MN_EXPORT bool
 	path_is_folder(const char* path);
 
-	/**
-	 * @brief      Returns whether the given path is a folder or not
-	 */
+	// returns whether the given path is a folder
 	inline static bool
 	path_is_folder(const Str& path)
 	{
 		return path_is_folder(path.ptr);
 	}
 
-	/**
-	 * @brief      Returns whether the given path is a file or not
-	 */
+	// returns whether the given path is a file
 	MN_EXPORT bool
 	path_is_file(const char* path);
 
-	/**
-	 * @brief      Returns whether the given path is a file or not
-	 */
+	// returns whether the given path is a file
 	inline static bool
 	path_is_file(const Str& path)
 	{
 		return path_is_file(path.ptr);
 	}
 
-	/**
-	 * @brief      Returns the current path of the process
-	 *
-	 * @param[in]  allocator  The allocator to be used in the returned string
-	 */
+	// returns the current working path of the process
 	MN_EXPORT Str
 	path_current(Allocator allocator = allocator_top());
 
-	/**
-	 * @brief      Changes the current path of the process
-	 *
-	 * @param[in]  path  The path
-	 */
+	// changes the current working path of the process
 	MN_EXPORT void
 	path_current_change(const char* path);
 
-	/**
-	 * @brief      Changes the current path of the process
-	 *
-	 * @param[in]  path  The path
-	 */
+	// changes the current working path of the process
 	inline static void
 	path_current_change(const Str& path)
 	{
 		path_current_change(path.ptr);
 	}
 
-	/**
-	 * @brief      Returns the absolute path of the given relative path
-	 *
-	 * @param[in]  path       The relative path
-	 * @param[in]  allocator  The allocator to be used in the returns string
-	 */
+	// returns the absolute path of the given relative path
 	MN_EXPORT Str
 	path_absolute(const char* path, Allocator allocator = allocator_top());
 
-	/**
-	 * @brief      Returns the absolute path of the given relative path
-	 *
-	 * @param[in]  path       The relative path
-	 * @param[in]  allocator  The allocator to be used in the returns string
-	 */
+	// returns the absolute path of the given relative path
 	inline static Str
 	path_absolute(const Str& path, Allocator allocator = allocator_top())
 	{
 		return path_absolute(path.ptr, allocator);
 	}
 
+	// returns the directory of the given file path
 	MN_EXPORT Str
 	file_directory(const char* path, Allocator allocator = allocator_top());
 
+	// returns the directory of the given file path
 	inline static Str
 	file_directory(const Str& path, Allocator allocator = allocator_top())
 	{
 		return file_directory(path.ptr, allocator);
 	}
 
+	// represents a file system entity file/folder
 	struct Path_Entry
 	{
 		enum KIND
@@ -197,33 +143,25 @@ namespace mn
 		Str  name;
 	};
 
+	// frees the given path entry
 	inline static void
 	path_entry_free(Path_Entry& self)
 	{
 		str_free(self.name);
 	}
 
+	// destruct overload of path entry free
 	inline static void
 	destruct(Path_Entry& self)
 	{
 		path_entry_free(self);
 	}
 
-	/**
-	 * @brief      Returns the names of the children files/folders of the given path
-	 *
-	 * @param[in]  path       The path
-	 * @param[in]  allocator  The allocator to be used in the returned buf
-	 */
+	// returns the names of the children files/folders of the given path
 	MN_EXPORT Buf<Path_Entry>
 	path_entries(const char* path, Allocator allocator = allocator_top());
 
-	/**
-	 * @brief      Returns the names of the children files/folders of the given path
-	 *
-	 * @param[in]  path       The path
-	 * @param[in]  allocator  The allocator to be used in the returned buf
-	 */
+	// returns the names of the children files/folders of the given path
 	inline static Buf<Path_Entry>
 	path_entries(const Str& path, Allocator allocator = allocator_top())
 	{
@@ -234,12 +172,12 @@ namespace mn
 	MN_EXPORT Str
 	path_executable(Allocator allocator = allocator_top());
 
-	// file_last_write_time returns the time of the last write to the given file
+	// returns the time of the last write to the given file
 	// if path is not correct it will return 0
 	MN_EXPORT int64_t
 	file_last_write_time(const char* path);
 
-	// file_last_write_time returns the time of the last write to the given file
+	// returns the time of the last write to the given file
 	// if path is not correct it will return 0
 	inline static int64_t
 	file_last_write_time(const mn::Str& path)
@@ -248,239 +186,155 @@ namespace mn
 	}
 
 
-	/**
-	 * @brief      Copies a file from src to dst
-	 *
-	 * @param[in]  src   The source
-	 * @param[in]  dst   The destination
-	 */
+	// copies a file from src to dst, and returns whether the operation is successful
 	MN_EXPORT bool
 	file_copy(const char* src, const char* dst);
 
-	/**
-	 * @brief      Copies a file from src to dst
-	 *
-	 * @param[in]  src   The source
-	 * @param[in]  dst   The destination
-	 */
+	// copies a file from src to dst, and returns whether the operation is successful
 	inline static bool
 	file_copy(const Str& src, const char* dst)
 	{
 		return file_copy(src.ptr, dst);
 	}
 
-	/**
-	 * @brief      Copies a file from src to dst
-	 *
-	 * @param[in]  src   The source
-	 * @param[in]  dst   The destination
-	 */
+	// copies a file from src to dst, and returns whether the operation is successful
 	inline static bool
 	file_copy(const char* src, const Str& dst)
 	{
 		return file_copy(src, dst.ptr);
 	}
 
-	/**
-	 * @brief      Copies a file from src to dst
-	 *
-	 * @param[in]  src   The source
-	 * @param[in]  dst   The destination
-	 */
+	// copies a file from src to dst, and returns whether the operation is successful
 	inline static bool
 	file_copy(const Str& src, const Str& dst)
 	{
 		return file_copy(src.ptr, dst.ptr);
 	}
 
-	/**
-	 * @brief      Removes a file
-	 *
-	 * @param[in]  path  The file path
-	 */
+	// removes a file, and returns whether the operation is successful
 	MN_EXPORT bool
 	file_remove(const char* path);
 
-	/**
-	 * @brief      Removes a file
-	 *
-	 * @param[in]  path  The file path
-	 */
+	// removes a file, and returns whether the operation is successful
 	inline static bool
 	file_remove(const Str& path)
 	{
 		return file_remove(path.ptr);
 	}
 
-	/**
-	 * @brief      Moves a file from src to dst
-	 *
-	 * @param[in]  src   The source
-	 * @param[in]  dst   The destination
-	 */
+	// moves a file from src to dst, and returns whether the operation is successful
 	MN_EXPORT bool
 	file_move(const char* src, const char* dst);
 
-	/**
-	 * @brief      Moves a file from src to dst
-	 *
-	 * @param[in]  src   The source
-	 * @param[in]  dst   The destination
-	 */
+	// moves a file from src to dst, and returns whether the operation is successful
 	inline static bool
 	file_move(const Str& src, const char* dst)
 	{
 		return file_move(src.ptr, dst);
 	}
 
-	/**
-	 * @brief      Moves a file from src to dst
-	 *
-	 * @param[in]  src   The source
-	 * @param[in]  dst   The destination
-	 */
+	// moves a file from src to dst, and returns whether the operation is successful
 	inline static bool
 	file_move(const char* src, const Str& dst)
 	{
 		return file_move(src, dst.ptr);
 	}
 
-	/**
-	 * @brief      Moves a file from src to dst
-	 *
-	 * @param[in]  src   The source
-	 * @param[in]  dst   The destination
-	 */
+	// moves a file from src to dst, and returns whether the operation is successful
 	inline static bool
 	file_move(const Str& src, const Str& dst)
 	{
 		return file_move(src.ptr, dst.ptr);
 	}
 
-	/**
-	* @brief      Returns file name without the path
-	*
-	* @param[in]  path  The file path
-	*/
+	// returns the file name part of the given path
 	MN_EXPORT Str
 	file_name(const Str& path, Allocator allocator = allocator_top());
 
+	// returns the file name part of the given path
 	inline static Str
 	file_name(const char* path, Allocator allocator = allocator_top())
 	{
 		return file_name(str_lit(path), allocator);
 	}
 
+	// returns a new file path inside the temproray folder using the given base name and extension
+	// file_tmp("base", "png"): /tmp/base_3248239476.png
 	MN_EXPORT Str
 	file_tmp(const Str& base, const Str& ext, Allocator allocator = allocator_top());
 
+	// returns a new file path inside the temproray folder using the given base name and extension
+	// file_tmp("base", "png"): /tmp/base_3248239476.png
 	inline static Str
 	file_tmp(const char* base, const Str& ext, Allocator allocator = allocator_top())
 	{
 		return file_tmp(str_lit(base), ext, allocator);
 	}
 
+	// returns a new file path inside the temproray folder using the given base name and extension
+	// file_tmp("base", "png"): /tmp/base_3248239476.png
 	inline static Str
 	file_tmp(const Str& base, const char* ext, Allocator allocator = allocator_top())
 	{
 		return file_tmp(base, str_lit(ext), allocator);
 	}
 
+	// returns a new file path inside the temproray folder using the given base name and extension
+	// file_tmp("base", "png"): /tmp/base_3248239476.png
 	inline static Str
 	file_tmp(const char* base, const char* ext, Allocator allocator = allocator_top())
 	{
 		return file_tmp(str_lit(base), str_lit(ext), allocator);
 	}
 
-	/**
-	 * @brief      Creates a new folder
-	 *
-	 * @param[in]  path  The folder path
-	 */
+	// creates a new folder and returns whether it succeeded
 	MN_EXPORT bool
 	folder_make(const char* path);
 
-	/**
-	 * @brief      Creates a new folder
-	 *
-	 * @param[in]  path  The folder path
-	 */
+	// creates a new folder and returns whether it succeeded
 	inline static bool
 	folder_make(const Str& path)
 	{
 		return folder_make(path.ptr);
 	}
 
-	/**
-	 * @brief      Removes a folder and the contained files/folders
-	 *
-	 * @param[in]  path  The folder path
-	 */
+	// removes a new folder and returns whether it succeeded
 	MN_EXPORT bool
 	folder_remove(const char* path);
 
-	/**
-	 * @brief      Removes a folder and the contained files/folders
-	 *
-	 * @param[in]  path  The folder path
-	 */
+	// removes a new folder and returns whether it succeeded
 	inline static bool
 	folder_remove(const Str& path)
 	{
 		return folder_remove(path.ptr);
 	}
 
-	/**
-	 * @brief      Copies a folder and the contained files/folders from src to dst
-	 *
-	 * @param[in]  src   The source
-	 * @param[in]  dst   The destination
-	 */
+	// copies a folder and the contained files/folders from src to dst, and returns whether it succeeded
 	MN_EXPORT bool
 	folder_copy(const char* src, const char* dst);
 
-	/**
-	 * @brief      Copies a folder and the contained files/folders from src to dst
-	 *
-	 * @param[in]  src   The source
-	 * @param[in]  dst   The destination
-	 */
+	// copies a folder and the contained files/folders from src to dst, and returns whether it succeeded
 	inline static bool
 	folder_copy(const Str& src, const char* dst)
 	{
 		return folder_copy(src.ptr, dst);
 	}
 
-	/**
-	 * @brief      Copies a folder and the contained files/folders from src to dst
-	 *
-	 * @param[in]  src   The source
-	 * @param[in]  dst   The destination
-	 */
+	// copies a folder and the contained files/folders from src to dst, and returns whether it succeeded
 	inline static bool
 	folder_copy(const char* src, const Str& dst)
 	{
 		return folder_copy(src, dst.ptr);
 	}
 
-	/**
-	 * @brief      Copies a folder and the contained files/folders from src to dst
-	 *
-	 * @param[in]  src   The source
-	 * @param[in]  dst   The destination
-	 */
+	// copies a folder and the contained files/folders from src to dst, and returns whether it succeeded
 	inline static bool
 	folder_copy(const Str& src, const Str& dst)
 	{
 		return folder_copy(src.ptr, dst.ptr);
 	}
 
-	/**
-	 * @brief      Moves a folder and the contained files/folders from src to dst
-	 *
-	 * @param[in]  src   The source
-	 * @param[in]  dst   The destination
-	 */
+	// moves a folder and the contained files/folders from src to dst, and returns whether it succeeded
 	inline static bool
 	folder_move(const char* src, const char* dst)
 	{
@@ -489,50 +343,32 @@ namespace mn
 		return false;
 	}
 
-	/**
-	 * @brief      Moves a folder and the contained files/folders from src to dst
-	 *
-	 * @param[in]  src   The source
-	 * @param[in]  dst   The destination
-	 */
+	// moves a folder and the contained files/folders from src to dst, and returns whether it succeeded
 	inline static bool
 	folder_move(const Str& src, const char* dst)
 	{
 		return folder_move(src.ptr, dst);
 	}
 
-	/**
-	 * @brief      Moves a folder and the contained files/folders from src to dst
-	 *
-	 * @param[in]  src   The source
-	 * @param[in]  dst   The destination
-	 */
+	// moves a folder and the contained files/folders from src to dst, and returns whether it succeeded
 	inline static bool
 	folder_move(const char* src, const Str& dst)
 	{
 		return folder_move(src, dst.ptr);
 	}
 
-	/**
-	 * @brief      Moves a folder and the contained files/folders from src to dst
-	 *
-	 * @param[in]  src   The source
-	 * @param[in]  dst   The destination
-	 */
+	// moves a folder and the contained files/folders from src to dst, and returns whether it succeeded
 	inline static bool
 	folder_move(const Str& src, const Str& dst)
 	{
 		return folder_move(src.ptr, dst.ptr);
 	}
 
+	// returns the path of the temporary folder
 	MN_EXPORT Str
 	folder_tmp(Allocator allocator = allocator_top());
 
-	/**
-	 * @brief      Returns the system config folder
-	 *
-	 * @param[in]  allocator   The allocator to be used in the returned str
-	 */
+	// returns the system configuration folder
 	MN_EXPORT Str
 	folder_config(Allocator allocator = allocator_top());
 }
