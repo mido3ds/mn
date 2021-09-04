460ce8a423052444fd7e6176506d4c4e447da085
Mostafa Saad <moustapha.saad.abdelhamed@gmail.com>
2021-09-04 10:34:00 +0200

    fix macos backtrace generator (#69)

--- mn/src/mn/mac/Debug.cpp
+++ mn/src/mn/mac/Debug.cpp
@@ -19,49 +19,27 @@ namespace mn
 	callstack_print_to([[maybe_unused]] void** frames, [[maybe_unused]] size_t frames_count, [[maybe_unused]] mn::Stream out)
 	{
 #ifndef NDEBUG
-		constexpr size_t MAX_NAME_LEN = 255;
-		//+1 for null terminated string
-		char name_buffer[MAX_NAME_LEN+1];
 		char** symbols = backtrace_symbols(frames, frames_count);
 		if(symbols)
 		{
 			for(size_t i = 0; i < frames_count; ++i)
 			{
-				//isolate the function name
-				char *name_begin = nullptr, *name_end = nullptr, *name_it = symbols[i];
-				while(*name_it != 0)
-				{
-					if(*name_it == '(')
-						name_begin = name_it+1;
-					else if(*name_it == ')' || *name_it == '+')
-					{
-						name_end = name_it;
-						break;
-					}
-					++name_it;
-				}
+				// example output
+				// 0   <module_name>     0x0000000000000000 function_name + 00
+				char function_name[1024] = {};
+				char address[48] = {};
+				char module_name[1024] = {};
+				int offset = 0;
 
-				
-				size_t mangled_name_size = name_end - name_begin;
-				//function maybe inlined
-				if(mangled_name_size == 0)
-				{
-					mn::print_to(out, "[{}]: {}\n", frames_count - i - 1, symbols[i]);
-					continue;
-				}
-
-				//copy the function name into the name buffer
-				size_t copy_size = mangled_name_size > MAX_NAME_LEN ? MAX_NAME_LEN : mangled_name_size;
-				memcpy(name_buffer, name_begin, copy_size);
-				name_buffer[copy_size] = 0;
+				::sscanf(symbols[i], "%*s %s %s %s %*s %d", module_name, address, function_name, &offset);
 
 				int status = 0;
-				char* demangled_name = abi::__cxa_demangle(name_buffer, NULL, 0, &status);
+				char* demangled_name = abi::__cxa_demangle(function_name, NULL, 0, &status);
 
 				if(status == 0)
 					mn::print_to(out, "[{}]: {}\n", frames_count - i - 1, demangled_name);
 				else
-					mn::print_to(out, "[{}]: {}\n", frames_count - i - 1, name_buffer);
+					mn::print_to(out, "[{}]: {}\n", frames_count - i - 1, function_name);
 
 				::free(demangled_name);
 			}
